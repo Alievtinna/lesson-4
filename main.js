021de08d7dbacea32552885fa1b4a9233142243d
Alla <allaabdulina705@gmail.com>
2025-05-28 19:37:08 +0500

    end

---
+++ main.js
@@ -0,0 +1,34 @@
+// let year = prompt ("Когда вышел ходячий замок Хаяо ǿиядзаки?")
+// if (year == 2004) {
+//   alert ("Да вы знаток!");
+// } else {
+//   alert ("А вот и неправильно!");
+// }
+
+// let year = prompt ('Когда вышел ходячий замок Хаяо ǿиядзаки?');
+// if (year < 2004) {
+//  alert( 'Рано' );
+// } else if (year > 2004) {
+//  alert( 'Поздно' );
+// } else {
+//  alert( 'Верно!' );
+// }
+
+// year == 2004
+// ? alert (true)
+// : alert (false)
+
+// let summ = prompt ("a+b")
+// if (summ > 15) {
+//   alert ("Yes");
+// } else {
+//   alert ("No");
+// }
+
+let a = 5
+let b = 6 
+console.log (a==b)
+console.log (a!=b)
+console.log (a**b)
+console.log (a%b)
+console.log (a/b)

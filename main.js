42566fd1e557a4eb7c2f04b7dac333b53c7e1d57
Алла <156201723+Alievtinna@users.noreply.github.com>
2025-06-01 21:13:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let summ = prompt ("a+b")
+if (summ > 15) {
+  alert ("Yes");
+} else {
+  alert ("No");
+}
+
+let a = 5
+let b = 6 
+if resut (a == b) {
+  console.log (true)
+} else (a!=b) {
+  console.log (false)
+}
